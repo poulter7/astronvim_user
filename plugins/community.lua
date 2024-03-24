36eb55cedcb16eadbdffb82ac58f2c78cf2bd626
Jonathan Poulter <poulter7@gmail.com>
2024-03-24 11:00:28 -0500

    community plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,14 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
+  { import = "astrocommunity.debugging.nvim-dap-repl-highlights" },
+  { import = "astrocommunity.debugging.telescope-dap-nvim" },
+  { import = "astrocommunity.lsp.lsp-signature-nvim" },
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.scrolling.neoscroll-nvim" },
+  { import = "astrocommunity.scrolling.nvim-scrollbar" },
+  -- { import = "astrocommunity.lsp.inc-rename-nvim" },
+
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
