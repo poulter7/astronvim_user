8da39e7f6533c838e1e4dfe4baa1f701618c13fd
Jonathan Poulter <poulter7@gmail.com>
2024-03-30 02:00:47 -0500

    Update community.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -31,11 +31,11 @@ return {
             runtimes = {
               {
                 name = "JavaSE-17",
-                path = "/opt/homebrew/opt/openjdk@17/libexec/openjdk.jdk/Contents/Home/bin/",
+                path = "/opt/homebrew/opt/openjdk@17/libexec/openjdk.jdk/Contents/Home/",
               },
               {
                 name = "JavaSE-21",
-                path = "/opt/homebrew/opt/openjdk/libexec/openjdk.jdk/Contents/Home/bin/",
+                path = "/opt/homebrew/opt/openjdk/libexec/openjdk.jdk/Contents/Home/",
               }
             },
           },
