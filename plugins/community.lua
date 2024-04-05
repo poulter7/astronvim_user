8507123e77bf852c77f784a4405168af1f5b7049
Jonathan Poulter <poulter7@gmail.com>
2024-04-05 08:33:25 -0500

    Update community.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -77,12 +77,25 @@ return {
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.julia" },
   { import = "astrocommunity.pack.toml" },
-
   {
     "zbirenbaum/copilot.lua",
     opts = {
       suggestion = { enabled = true, auto_trigger = true, keymap = {accept = "<C-l>"}},
       panel = { enabled = false}
     }
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    config = function(plugin, opts)
+      -- run default AstroNvim nvim-dap-ui configuration function
+      require("plugins.configs.nvim-dap-ui")(plugin, opts)
+  
+      -- disable dap events that are created
+      local dap = require("dap")
+      
+      -- dap.listeners.after.event_initialized["dapui_config"] = nil
+      dap.listeners.before.event_terminated["dapui_config"] = nil
+      dap.listeners.before.event_exited["dapui_config"] = nil
+    end,
   }
 }
